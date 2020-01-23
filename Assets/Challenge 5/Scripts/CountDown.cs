9fdace588e2030e9be588bb91662c88340d0bb6f
rick325601 <56115492+rick325601@users.noreply.github.com>
2020-01-23 11:10:32 +0100

    no message

---
+++ Assets/Challenge 5/Scripts/CountDown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class CountDown : MonoBehaviour
+{
+    public int timeLeft = 60;
+    public Text countdown;
+    private GameManagerX gameManagerX;
+
+    void Start()
+    {
+     
+    }
+    public void StartTimer ()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+    }
+    
+    void Update () 
+    {
+        countdown.text = ("time: " + timeLeft);
+        
+        if (timeLeft <= 0)
+        {
+            gameManagerX.GameOver();
+        }
+    }
+    
+    //Simple Coroutine
+    IEnumerator LoseTime()
+    {
+        while (true) {
+            yield return new WaitForSeconds (1);
+            timeLeft--; 
+        }
+    }
+}
